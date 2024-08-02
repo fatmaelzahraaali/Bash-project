4062587c7b8daa984d792d52d28a2a346cff9471
fatmaelzahraaali <fatma.elzahraa.ali.75@gmail.com>
2024-08-03 01:23:02 +0300

    create database and testing

--- functions/functions.sh
+++ functions/functions.sh
@@ -7,9 +7,10 @@ RED='\033[1;31m'	          # Red Color code
 Green='\033[1;32m'	        # Green Color Green
 ColorReset='\033[0m' 		    # No Color Code
 
+### main_menu ###
 
-############### main_menu ##########################
-#this function displays the main menu with GUI 
+### this function displays the main menu with GUI ### 
+
 mainMenu()
 {
      choice=$(zenity --list \
@@ -31,13 +32,37 @@ mainMenu()
      fi
  
      case $choice in 
-		"Create Database") ./main_menu/create_db.sh;;
-		"List Database") ./main_menu/list_db.sh;;
-		"Connect To Database") ./main_menu/connect_db.sh;;
-		"Drop Database") ./main_menu/drop_db.sh;;
+		"Create Database") ./options/create_db.sh;;
+		"List Database") ./options/list_db.sh;;
+		"Connect To Database") ./options/connect_db.sh;;
+		"Drop Database") ./options/drop_db.sh;;
                 "Exit") echo -e "${Green}Exited..${ColorReset}";
 		exit;; #exit from database
-		*) echo -e "${RED}invalid choice, try again ... you must choose                  only from the above list${ColorReset}";
-	         mainMenu          #Call it again
+		*) echo -e "${RED}invalid choice, try again ... you must choose only from the above list${ColorReset}";
+
+        	mainMenu          #Call it again
        esac
+}
+
+
+### this function checks if the database already exist or not ####
+
+isDatabaseExist()
+{
+  if [ -d ./Database/$1 ]
+  then
+	  return 0          #returns  0 if true (exist)
+  else
+	  return 1          #returns 1 if false (not exist)
+  fi
+
+}
+### this function creates database ####
+
+createDatabase()
+{
+
+  mkdir ./Database/$1
+  mkdir ./Database/$1/.metadata
+
 }
---
+++ options/create_db.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+
+. ./functions/functions.sh
+
+while true
+do
+  
+  dbName=$(zenity --entry \
+  --title="Add new database" \
+  --text="Enter database name:" \
+  --entry-text "ITI-DB")
+
+  if [[ -z "$dbName" ]] || [[ ! $dbName =~  ^[a-zA-Z]+[a-zA-Z0-9]*$ ]] 
+  then
+      zenity --error --width="300" --text="Database field cannot be empty or start with space or number or special         char"
+  else
+      # check if the database is exit or not
+      if isDatabaseExist $dbName
+      then
+          zenity --error --width="200" --text="[$dbName] is already exist"   
+      else
+          createDatabase $dbName
+
+          # check if last command is Done(a datebase name is entered successfully
+          if [ $? -eq 0 ]
+          then
+              
+              zenity --info --width="200" --text="[$dbName] created successfully"
+              break
+          else
+             
+              zenity --error --width="200" --text="Error occured during creating the database"   
+
+          fi
+
+        fi
+      fi 
+done
+
+mainMenu
